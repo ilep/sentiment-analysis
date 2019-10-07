e57e6d7559e3a6061b53dc08aa487203063edfd0
skad94 <skakamta@yahoo.fr>
2019-10-07 12:25:59 +0200

    enhanced the naive nlp algo
    enhance + little cleaning.
    still somme cleaning to do and clarify include

--- test_nlp.py
+++ test_nlp.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 """
 Created on Wed Oct  2 12:06:07 2019
 
@@ -8,26 +8,29 @@ import pandas as pd
 from past.builtins import execfile
 import os
 
-def include(filename):
-    if os.path.exists(filename): 
-        execfile(filename)
 include('C:\\Users\\Skad\\.spyder-py3\\test_nlp_classif.py')
 
-ref_positive = ('like', 'love', 'great', 'good','interesting', 'cool')
-ref_negative = ('overated', 'bad', 'sad', 'worst', 'wrong')
+#ref_positive = frozenset('like', 'love', 'great', 'good','interesting', 'cool')
+#ref_negative = frozenset('overated', 'bad', 'sad', 'worst', 'wrong')
     
 ###############################################################"
 df = pd.read_csv('C:\\Users\\Skad\\Desktop\\Python\\testdata.txt',sep='delimiter',names=['txt'],engine='python')
 # engin python is to let the ""compiler"" know that the C parser is not needed, that enable a warning Parserwarning
-Tweets = df['txt']
+
+#Tweets = df['txt']
+Tweets = [df['txt'][0],df['txt'][1],df['txt'][2],df['txt'][3],df['txt'][5],df['txt'][94]]
 arraySplitTweet = []
 splitTweet(Tweets, arraySplitTweet)
+#print(arraySplitTweet)
 
 ##### Tweets that contain "like"
 arrayLike = []
+arrayLove = []
 fun('like', arraySplitTweet, arrayLike)
-print(arrayLike)
-print("ici")
+fun('love', arraySplitTweet, arrayLove)
+print("array like\n", arrayLike)
+print("array love\n", arrayLove)
+print(df['txt'][4])
 
 #print(arraySplitTweet)
 
--- test_nlp_classif.py
+++ test_nlp_classif.py
@@ -5,15 +5,23 @@ Created on Fri Oct  4 16:38:57 2019
 @author: Skad
 """
 
-def fun(wordToSearch, arrayData_txt, arrayRes):
+def include(filename):
+    if os.path.exists(filename): 
+        execfile(filename)
+
+def fun(wordToSearch, arrayData_txt, arrayRes, weigh = 1):
+    #Loop in the data , everytime the wordToSearch is find the output is incremented with weigh
     i = 0    
     for x in arrayData_txt:
-        if (wordToSearch in arrayData_txt):
-            arrayRes.append(1)
+        y = frozenset(x)  #use of set in order to speed up the search
+        if (wordToSearch in y):
+            arrayRes.append(weigh)
         else:
             arrayRes.append(0)
         i = 1 + i
         
 def splitTweet(pdDataColumn, arraySplitTweet = []):
+    #
     for x in pdDataColumn:
+       # y = str(x).lower()
         arraySplitTweet.append(str(x).split())    
